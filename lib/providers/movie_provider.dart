58b7bc8eb4d951cf05c3cf324940c98cf900cf6e
unknown <shazniainun@gmail.com>
2023-05-19 09:23:17 +0800

    first commit

---
+++ lib/providers/movie_provider.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+import '../models/movie_model.dart';
+import 'package:flutter/cupertino.dart';
+
+final List<Movie> initialData = List.generate(
+    50,
+        (index) => Movie(
+          title: "Movie $index",
+          duration: "${Random().nextInt(100) + 60} minutes")
+);
+
+class MovieProvider with ChangeNotifier{
+
+  final List<Movie> _movies =initialData;
+  List<Movie> get movies => _movies;
+
+  final List<Movie> _myList =[];
+  List<Movie> get myList => _myList;
+
+  void addToList(Movie movie){
+    _myList.add(movie);
+    notifyListeners();
+  }
+
+  void removeFromList(Movie movie){
+    _myList.remove(movie);
+    notifyListeners();
+  }
+
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:providerlist/screens/fav_movies.dart';
+import '../providers/movie_provider.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context){
+    var movies = context.watch<MovieProvider>().movies;
+    var myList = context.watch<MovieProvider>().myList;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movies Provider'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton.icon(
+              onPressed: (){
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context)=>MyListScreen()
+                  ),
+                );
+              },
+              icon: const Icon(Icons.favorite),
+              label: Text(
+                "Go to my list (${myList.length})",
+                style: const TextStyle(fontSize: 24),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                padding: const EdgeInsets.symmetric(vertical: 20)),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: movies.length,
+                itemBuilder: (_, index) {
+                  final currentMovie = movies[index];
+                  return Card(
+                    key: ValueKey(currentMovie.title),
+                    color: Colors.blue,
+                    elevation: 4,
+                    child: ListTile(
+                      title: Text(currentMovie.title,
+                        style: const TextStyle(
+                            color: Colors.white
+                        ),),
+                      subtitle:
+                      Text(currentMovie.duration ?? 'No information',
+                        style: const TextStyle(
+                            color: Colors.white
+                        ),),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.favorite,
+                          color: myList.contains(currentMovie)
+                              ? Colors.red
+                              : Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: (){
+                          if (!myList.contains(currentMovie)){
+                            context
+                              .read<MovieProvider>()
+                                .addToList(currentMovie);
+                          } else {
+                            context
+                                .read<MovieProvider>()
+                                .removeFromList(currentMovie);
+                          }
+                        },
+
+                      ),
+                    ),
+                  );
+                }
+              ),
+            ),
+          ],
+        ),
+    ),
+    );
+    }
+  }
